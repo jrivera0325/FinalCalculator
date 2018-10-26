5e2aaef058e22e549e5819c3b18345f4fb3bff52
Julio Rivera <jrivera0325@jh-mba158-16.d214.org>
2018-10-26 11:17:27 -0500

    layout

--- FinalCalculator/ViewController.swift
+++ FinalCalculator/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var percentNeeded: UILabel!
+    @IBOutlet weak var textFieldOne: UITextField!
+    @IBOutlet weak var textFieldTwo: UITextField!
+    @IBOutlet weak var textFieldThree: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    @IBAction func calculate(_ sender: UIButton) {
+        let currentGrade = Int(textFieldOne.text!)!
+        let desiredGrade = Int(textFieldTwo.text!)!
+        let testPercent = Int(textFieldThree.text!)!
+        let answer = (0.1 * desiredGrade - (100 - testPercent) * currentGrade) / desiredGrade
+        
+        percentNeeded.text! = ("\(answer)")
+        
+        
+    }
+    
+    
 }
 
